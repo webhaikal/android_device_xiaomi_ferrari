4360f42c6bd51b940c0c60ecdf537bf1469a4c94
Haikal Izzuddin <heywhite69@gmail.com>
2016-12-19 12:03:07 +0800

    Revert "ferrari: Add XiaomiDoze package"
    This reverts commit 9bee69899ffade52ac523e4a4f643a9344fd9d9f.

--- doze/src/org/cyanogenmod/doze/ferrari/DozeService.java
+++ doze/src/org/cyanogenmod/doze/ferrari/DozeService.java
@@ -14,37 +14,44 @@
  * limitations under the License.
  */
 
-package com.cyanogenmod.settings.doze;
+package org.cyanogenmod.doze.ferrari;
 
+import android.app.Activity;
+import android.app.IntentService;
 import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.provider.Settings;
 import android.util.Log;
 
 public class DozeService extends Service {
-    private static final String TAG = "DozeService";
-    private static final boolean DEBUG = false;
 
-    private ProximitySensor mProximitySensor;
+    private static final boolean DEBUG = false;
+    private static final String TAG = "DozeService";
+
+    private Context mContext;
     private TiltSensor mTiltSensor;
+    private ProximitySensor mProximitySensor;
 
     @Override
     public void onCreate() {
         if (DEBUG) Log.d(TAG, "Creating service");
-        mProximitySensor = new ProximitySensor(this);
-        mTiltSensor = new TiltSensor(this);
-
-        IntentFilter screenStateFilter = new IntentFilter(Intent.ACTION_SCREEN_ON);
-        screenStateFilter.addAction(Intent.ACTION_SCREEN_OFF);
-        registerReceiver(mScreenStateReceiver, screenStateFilter);
+        super.onCreate();
+        mContext = this;
+        mTiltSensor = new TiltSensor(mContext);
+        mProximitySensor = new ProximitySensor(mContext);
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         if (DEBUG) Log.d(TAG, "Starting service");
+        IntentFilter screenStateFilter = new IntentFilter(Intent.ACTION_SCREEN_ON);
+        screenStateFilter.addAction(Intent.ACTION_SCREEN_OFF);
+        mContext.registerReceiver(mScreenStateReceiver, screenStateFilter);
         return START_STICKY;
     }
 
@@ -52,9 +59,8 @@ public class DozeService extends Service {
     public void onDestroy() {
         if (DEBUG) Log.d(TAG, "Destroying service");
         super.onDestroy();
-        this.unregisterReceiver(mScreenStateReceiver);
-        mProximitySensor.disable();
         mTiltSensor.disable();
+        mProximitySensor.disable();
     }
 
     @Override
@@ -62,24 +68,21 @@ public class DozeService extends Service {
         return null;
     }
 
+    private boolean isDozeEnabled() {
+        return Settings.Secure.getInt(mContext.getContentResolver(),
+            Settings.Secure.DOZE_ENABLED, 1) != 0;
+    }
+
     private void onDisplayOn() {
         if (DEBUG) Log.d(TAG, "Display on");
-        if (Utils.pickUpEnabled(this)) {
-            mTiltSensor.disable();
-        }
-        if (Utils.handwaveGestureEnabled(this) ||
-                Utils.pocketGestureEnabled(this)) {
-            mProximitySensor.disable();
-        }
+        mTiltSensor.disable();
+        mProximitySensor.disable();
     }
 
     private void onDisplayOff() {
         if (DEBUG) Log.d(TAG, "Display off");
-        if (Utils.pickUpEnabled(this)) {
+        if (isDozeEnabled()) {
             mTiltSensor.enable();
-        }
-        if (Utils.handwaveGestureEnabled(this) ||
-                Utils.pocketGestureEnabled(this)) {
             mProximitySensor.enable();
         }
     }
@@ -87,10 +90,10 @@ public class DozeService extends Service {
     private BroadcastReceiver mScreenStateReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
-                onDisplayOn();
-            } else if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
+            if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
                 onDisplayOff();
+            } else if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
+                onDisplayOn();
             }
         }
     };
